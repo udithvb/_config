81d53fff62da0c6dd93cb00aa392d252bc415ddb
shenron <udithvb@gmail.com>
2019-03-18 18:36:32 +0000

    cleaner code and useful comments

--- yt_rss.py
+++ yt_rss.py
@@ -2,31 +2,17 @@ from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 import xml.dom.minidom as md
 
+#read rendered html
 with open("cmp.html",'r') as ytf:
     html_doc = ytf.read() 
 
 sp = BeautifulSoup(html_doc,'lxml')
 mydivs = sp.find_all("a",id="video-title")
-
-
-
-# dom = md.parse("sample.txt")
-# prettyxml = dom.toprettyxml()
-# with open("sample_pretty.xml",'x') as xml_file:
-#     xml_file.write(prettyxml)
+#mydivs contains all anchor tags with video title and url
 
 root = ET.parse("sample.xml").getroot()
-
-# for child in root:
-#     print(child.tag)
-
-
-# print(mydivs[0])
-#print("root 0 2: ",root[0][2]) #item
 item_ls = root[0]
-# print("item_ls:",item_ls)
-#root = ET.Element("channel")
-#item = ET.SubElement(item_ls,"item")
+#sample.xml contains boiler plate
 
 for count,i in enumerate(mydivs,1):
     item = ET.SubElement(item_ls,"item")
@@ -34,14 +20,11 @@ for count,i in enumerate(mydivs,1):
     ET.SubElement(item,"link").text = f"http://www.youtube.com{i['href']}"
     ET.SubElement(item,"description").text = str(count)
 
-    
-    # print(count,f"http://www.youtube.com{i['href']}")
-    # print(i.get_text())
-    #break
-
 tree =  ET.ElementTree(root)
 tree.write("filename.xml")
 
-# print(ET.tostring(root))
-# with open("test_xml",'x') as f:
-#     f.write(root.tostring())
+#for readable , pretty xml
+# dom = md.parse("sample.txt")
+# prettyxml = dom.toprettyxml()
+# with open("sample_pretty.xml",'x') as xml_file:
+#     xml_file.write(prettyxml)
